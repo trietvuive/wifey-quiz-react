98799330b3c0e401b7a845dffb6b8fc3c2de6f8e
trietvuive <vominhtriet02@gmail.com>
2025-01-01 03:26:32 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Quiz from './components/quiz/Quiz'
+import Header from './components/header/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Quiz></Quiz>
+    </div>
+  )
+}
+
+export default App
